30a661ddeb0a197bcbf0944a3a9170dcff1b25a8
Vitaliy Zhukov <DragonZX@users.noreply.github.com>
2026-05-26 14:04:39 +0300

    Match room with env, TODO cleanup

--- index.php
+++ index.php
@@ -30,11 +30,7 @@ $version="beta1";
     </style>
     </head>
     <body>
-      <!-- DEBUG START -->
-<div id="debug" style="display: none" class="alert alert-danger" role="alert">
-  <?php if(EnvIsSet("DEBUG_MODE", '', false)){echo ini_local('Debug enabled'); }?>
-</div>
-<!-- DEBUG END -->
+
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
   <a class="navbar-brand" href="<?php echo (EnvIsSet('URI_PROTO','','https').'://'. EnvIsSet('BASE_URI','','localhost'))?>"><img id="logo" alt="logo" src="<?php echo EnvIsSet('OEM_LOGO','','images/no_avatar.png') ?>" ></a>
@@ -95,7 +91,7 @@ $version="beta1";
 
     <div class="mb-3">
     <label for="InputRoom" class="form-label"><?php echo ini_local('Room name'); ?></label>
-    <input type="text" class="form-control form-control-lg" name="InputRoom" id="InputRoom" placeholder="public" aria-describedby="RoomHelp">
+    <input type="text" class="form-control form-control-lg" name="InputRoom" id="InputRoom" placeholder="<?php echo EnvIsSet('DEFAULT_ROOM','','public')?>" aria-describedby="RoomHelp">
     <div id="RoomHelp" class="form-text"><?php echo ini_local('Permitted room');?></div>
   </div>
     <div class="mb-3 form-check">
