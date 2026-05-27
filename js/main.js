36bfc54588454ca2bfca8cd08d09357c4c90bd0a
Vitaliy Zhukov <DragonZX@users.noreply.github.com>
2026-05-27 22:50:00 +0300

    no alert

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function copyJWTLink() {
     inputElement.select();
     inputElement.setSelectionRange(0, 99999);
     document.execCommand('copy');
-    alert("Copied the text: " + inputElement.value);
+    //alert("Copied the text: " + inputElement.value);
 }
 
 function copyJWToken() {
